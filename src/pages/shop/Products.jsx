20f5a1fc81c79f88eacfcabcd1628eced9b444ef
najmul57 <najmulcse247@gmail.com>
2026-01-10 22:41:19 +0600

    product filter

--- src/pages/shop/Products.jsx
+++ src/pages/shop/Products.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../../components/Button'
 import { products } from '../../utiles/products'
+import btnIcon from "../../assets/button-icon.png"
 import ProductCard from './ProductCard'
 
 const Products = ({ headline }) => {
     const categories = ["Chair", "Beds", 'Sofa', 'Lamps']
+    const [selectedCategory, setSelectedCategory] = useState('Sofa');
+    const [visibleProduct, setVisibleProduct] = useState(4);
+    const filteredProducts = products.filter((product) => product.category === selectedCategory);
+
+    const loadMoreProducts = () => {
+        setVisibleProduct((prev) => prev + 4)
+    }
+
     return (
         <section className='section-container'>
             <h2 className='text-4xl font-bold text-center my-8'>{headline}</h2>
@@ -13,17 +22,30 @@ const Products = ({ headline }) => {
                 <div className='flex flex-col sm:flex-row items-center md:justify-between justify-center gap-4'>
                     {
                         categories.map((category) => (
-                            <button key={category} className={`py-1.5 sm:px-5 px-8 rounded-full hover:bg-blue-500 hover:text-white transition-colors cursor-pointer`}>{category}</button>
+                            <button onClick={() => { setSelectedCategory(category); setVisibleProduct(4) }} key={category} className={`py-1.5 sm:px-5 px-8 rounded-full hover:bg-blue-500 hover:text-white transition-colors cursor-pointer ${selectedCategory === category ? 'bg-white text-blue-600' : ''}`}>{category}</button>
                         ))
                     }
                 </div>
             </div>
 
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6'>
+            <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
                 {
-                    products.map((product) => (
+                    filteredProducts.slice(0, visibleProduct).map((product) => (
                         <ProductCard products={product} />
                     ))
+                }
+            </div>
+
+            <div>
+                {
+                    visibleProduct < filteredProducts.length && (
+                        <div className='flex justify-between mt-3'>
+                            <button onClick={loadMoreProducts} className='flex gap-2 transition-all drop-shadow-blue-300 cursor-pointer hover:gap-4'>
+                                View All
+                                <img src={btnIcon} alt="" />
+                            </button>
+                        </div>
+                    )
                 }
             </div>
         </section>
